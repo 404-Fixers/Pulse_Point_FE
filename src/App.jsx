8082e101b422d1af572958f1431e11894eb24134
caleb042 <chibuikecalebobasi@gmail.com>
2026-08-18 23:05:06 +0100

    feat: create hospital dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import DonorDashboard from "./pages/DonorDashboard/DonorDashboard";
+import HospitalDashboard from "./pages/HospitalDashboard/HospitalDashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Landing Page */}
         <Route path="/" element={<LandingPage />} />
 
+        {/* Authentication */}
         <Route path="/login" element={<AuthPage />} />
         <Route path="/register" element={<AuthPage />} />
 
+        {/* Donor Dashboard */}
         <Route path="/dashboard/donor" element={<DonorDashboard />} />
+
+        {/* Hospital Dashboard */}
+        <Route path="/dashboard/hospital" element={<HospitalDashboard />} />
       </Routes>
     </BrowserRouter>
   );
