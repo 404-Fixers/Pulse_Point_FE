64f0681e8ef4e75819a88c5a128f21ba22756295
Gerald-Izuchukwu <geraldlouisugwunna@gmail.com>
2026-08-22 20:04:06 +0100

    editing form data to send what backend expects

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -53,7 +53,7 @@ function AuthPage() {
       endpoint = `${import.meta.env.VITE_API_URL}/auth/register/donor`;
 
       requestBody = {
-        fullName: formData.fullName,
+        name: formData.fullName,
         email: formData.email,
         password: formData.password,
         blood_type: formData.bloodType,
