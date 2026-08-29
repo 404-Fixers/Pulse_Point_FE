584f96a874ce9dbfef2e6d040aa83b4d0410e933
Gerald-Izuchukwu <geraldlouisugwunna@gmail.com>
2026-08-29 06:50:40 +0100

    fix: fixing create request units required

--- src/components/BloodRequestModal/BloodRequestModal.jsx
+++ src/components/BloodRequestModal/BloodRequestModal.jsx
@@ -185,27 +185,16 @@ function BloodRequestModal({ isOpen, onClose, onSubmit }) {
           <div className="blood-modal__field">
             <label htmlFor="unitsRequired">Units needed</label>
 
-            <select
+            <input
               id="unitsRequired"
-              name="units"
+              name="unitsRequired"
+              type="number"
               min="1"
-              // placeholder="e.g. 2"
+              placeholder="e.g. 2"
               value={formData.unitsRequired}
               onChange={handleChange}
               disabled={status === "loading"}
-            >
-              <option value="">Select Units Required</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            /</select>
+            />
 
             {errors.unitsRequired && (
               <span className="blood-modal__error">{errors.unitsRequired}</span>
